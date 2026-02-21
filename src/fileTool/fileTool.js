ef0baf9cbe456969cdbf5ae436505aefe11d531d
Andres Vaquero <web.andresvaquero@gmail.com>
2026-02-22 00:07:42 +1100

    disabing skipLibCheck

--- src/fileTool/fileTool.js
+++ src/fileTool/fileTool.js
@@ -1,6 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
+import { existsSync } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
+
+/**
+ * Find the first existing location from a list of possible locations.
+ * @param {string[]} locations - An array of file paths to check.
+ * @returns {string|null} The first existing file path, or null if none exist.
+ */
+export function findLocation(locations) {
+    return locations.find(loc => existsSync(loc)) || null;
+}
